d6566370f970fc7ae1569e04b5526871c4e04c4b
Tobias Frölich <froeltob@pm.me>
2024-07-24 08:25:58 +0200

    wip: deployment logs

--- src/k8s.rs
+++ src/k8s.rs
@@ -2,11 +2,26 @@ use anyhow::Ok;
 use colored::Colorize;
 use futures_util::AsyncBufReadExt;
 use futures_util::TryStreamExt;
+use k8s_openapi::api::apps::v1::Deployment;
 use k8s_openapi::api::core::v1::Pod;
 use kube::api::{Api, LogParams};
 use kube::ResourceExt;
 
 use crate::util;
+
+pub async fn get_pod_list_for_deployment(
+    client: &kube::Client,
+    deployment_name: &str,
+    ns_name: &str,
+) -> Result<(), anyhow::Error> {
+    let deployment_api: Api<Deployment> = Api::namespaced(client.clone(), ns_name);
+    let deployment = deployment_api.get(deployment_name).await?;
+
+    let spec = deployment.spec.unwrap();
+
+    // println!("{}", deployment.spec.unwrap().);
+    Ok(())
+}
 
 pub async fn stream_single_pod_logs(
     client: &kube::Client,
